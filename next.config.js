b0b999179d1c4743faf214acb35ef10005bc2ed0
Rachel Sequeira <63053089+rachel-ux@users.noreply.github.com>
2023-03-10 23:45:03 +0530

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  experimental: {
+    esmExternals: false,
+    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/*',
+        destination: 'http://localhost:5000/api/auth/*',
+      },
+    ]
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+}
